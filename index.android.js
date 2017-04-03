4a32092fe806dc4892a1e6e9ec1c2610f210a469
Ankit <ankit@niiu.de>
2017-04-03 17:07:03 +0530

    Vishnu is lord

--- index.android.js
+++ index.android.js
@@ -1,10 +1,11 @@
 import 'es6-symbol/implement';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import AppViewContainer from './src/modules/AppViewContainer';
-import React, {Component} from 'react';
-import {AppRegistry, BackAndroid,Platform} from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, BackAndroid, Platform } from 'react-native';
 import * as NavigationStateActions from './src/modules/dashboard/DashboardState';
+import {fromJS} from 'immutable';
 
 let listener = null
 
@@ -15,6 +16,15 @@ class DietcodeApp extends Component {
   }
 
   navigateBack() {
+    const navigationState = store.getState().get('dashboardState');
+    const tabs = navigationState.get('tabs');
+    const tabKey = tabs.getIn(['routes', tabs.get('index')]).get('key');
+    const currentTab = navigationState.get(tabKey);
+    // if we are in the beginning of our tab stack
+
+    if (currentTab.get('index') === 0) {
+      return false;
+    }
     store.dispatch(NavigationStateActions.popRoute());
     return true;
   }
